7de1349e774cc1a445baece33449807c1f6f0693
Lemursaurio <ggarro_1303@hotmail.com>
2024-11-07 12:05:48 -0500

    Commit inicial

---
+++ src/main/java/com/mycompany/ihc_interfaces/IHC_Interfaces.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ihc_interfaces;
+
+/**
+ *
+ * @author utente
+ */
+public class IHC_Interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
